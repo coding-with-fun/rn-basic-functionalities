6ccc57e50fd3425dfabd768bf5ec8d51787f8233
Coderc <harsh@coderc.in>
2021-04-16 23:14:55 +0530

    Added API call in Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {
+        axios
+            .get("https://api.quotable.io/random")
+            .then((res) => {
+                setTitle(res.data.content);
+            })
+            .catch(() => {
+                console.log("Error while fetching data");
+            });
+    }, []);
+
     return (
         <View>
-            <Text>Home</Text>
+            <Text>{title}</Text>
         </View>
     );
 };
